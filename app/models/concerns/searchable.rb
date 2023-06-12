59db3c243a4a90f4797cf2411c593b7cdb0ba060
Ben R <robinsonbena@gmail.com>
2023-06-12 15:22:47 -0700

    add different query methods to searchable concern and update the controller to use one

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -12,8 +12,76 @@ module Searchable
       indexes :genre, type: :keyword
     end
 
+    # basic query
     def self.search(query)
-      # build and run search
+      self.__elasticsearch__.search(query)
     end
+
+    def self.search_artist_field(query)
+      params = {
+        query: {
+          match: {
+            artist: query,
+          },
+        },
+      }
+
+      self.__elasticsearch__.search(params)
+    end
+
+    def self.search_multiple_fields(query)
+      params = {
+        query: {
+          multi_match: {
+            query: query, 
+            fields: [ :title, :artist, :lyrics ] 
+          },
+        },
+      }
+    
+      self.__elasticsearch__.search(params)
+    end
+
+    def self.search_and_filter_by_genre(query, genre = nil)
+      params = {
+        query: {
+          bool: {
+            must: [
+              {
+                multi_match: {
+                  query: query, 
+                  fields: [ :title, :artist, :lyrics ] 
+                }
+              },
+            ],
+            filter: [
+              {
+                term: { genre: genre }
+              }
+            ]
+          }
+        }
+      }
+  
+      self.__elasticsearch__.search(params)
+    end
+
+    # boosts the relevance score for artists matching query
+    def self.search_and_boost_artist(query)
+      params = {
+        query: {
+          bool: {
+            should: [
+              { match: { title: query }},
+              { match: { artist: { query: query, boost: 5 } }},
+              { match: { lyrics: query }},
+            ],
+          }
+        },
+      }
+  
+      self.__elasticsearch__.search(params)
+    end
+
   end
 end
